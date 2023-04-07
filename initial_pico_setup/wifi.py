6d5d817c3f9999a11b4285f07f83cbf03335ad69
Matt Graham <grahammatt33@gmail.com>
2023-04-07 00:58:28 -0500

    added mac address retrieval to wifi script

--- initial_pico_setup/wifi.py
+++ initial_pico_setup/wifi.py
@@ -1,4 +1,5 @@
 import network
+import ubinascii
 
 ssid = 'ICS 23 Twenty'
 # specific to the Pico Matt and Sam were working on as of 4/5/23, registered under Matt's wifi account
@@ -7,6 +8,9 @@ password = 'bzYqqdBD'
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
+
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+print(mac)
 
 while not wlan.isconnected():
     pass
